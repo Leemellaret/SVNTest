5da9042137403f14a3d053b67001c65990f3b7df
Artem212 <44548983+Artem212@users.noreply.github.com>
2021-02-28 00:43:28 +0500

    test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Testing SVN!");
+            Console.WriteLine("Tested SVN!");
         }
     }
 }
