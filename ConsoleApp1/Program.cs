6042e69787ab9f44d86ec1a7967b95727bc573c3
Leemellaret <38433380+Leemellaret@users.noreply.github.com>
2021-02-28 00:52:53 +0500

    test 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Tested SVN!");
+            Console.WriteLine("Tested twice SVN!");
         }
     }
 }
